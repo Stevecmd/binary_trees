9f2915d3afb7ec20512fa473de9f2c74f7cbb4ff
Stevecmd <murimi101@gmail.com>
2024-06-04 08:36:28 +0300

    betty check

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,15 +11,15 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    /* Traverse the left subtree */
-    binary_tree_postorder(tree->left, func);
+	/* Traverse the left subtree */
+	binary_tree_postorder(tree->left, func);
 
-    /* Traverse the right subtree */
-    binary_tree_postorder(tree->right, func);
+	/* Traverse the right subtree */
+	binary_tree_postorder(tree->right, func);
 
-    /* Call the function for the current node */
-    func(tree->n);
+	/* Call the function for the current node */
+	func(tree->n);
 }
