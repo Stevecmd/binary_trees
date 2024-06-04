b56e224fa451d43321ef243a4ee97e4bc1458b4d
Stevecmd <murimi101@gmail.com>
2024-06-04 08:15:39 +0300

    5-binary_tree_is_root.c

--- binary_trees.h
+++ binary_trees.h
@@ -39,4 +39,7 @@ void binary_tree_delete(binary_tree_t *tree);
 /* 4. Check if a node is a leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* 5. Check if a node is a root */
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif /* _BINARY_TREES_H_ */
