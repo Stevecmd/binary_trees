02cfe0aae4634eafadbdd77a557c600b308e8c00
Stevecmd <murimi101@gmail.com>
2024-06-05 12:52:49 +0300

    18-binary_tree_uncle.c

--- binary_trees.h
+++ binary_trees.h
@@ -67,7 +67,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 
 /* 14. Balance Factor */
-size_t binary_tree_height(const binary_tree_t *tree);
+/* size_t binary_tree_height(const binary_tree_t *tree); */
 int binary_tree_balance(const binary_tree_t *tree);
 
 /* 15. Is full */
@@ -84,3 +84,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* 18. Uncle */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
+
