1d3255870b96f186b7bef0ae64c6b3141e25ae20
Stevecmd <murimi101@gmail.com>
2024-06-05 20:43:57 +0300

    14-binary_tree_balance.c update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,13 +11,13 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+		return (
+			(int)binary_tree_height(tree->left) -
+			(int)binary_tree_height(tree->right)
+		);
 
-	int left_height = (int)binary_tree_height(tree->left);
-	int right_height = (int)binary_tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (0);
 }
 
 /**
