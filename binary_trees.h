936e5966b68b8ed4c2014d8458341eb6a1fecee8
Stevecmd <murimi101@gmail.com>
2024-06-05 12:01:31 +0300

    17-binary_tree_sibling.c

--- binary_trees.h
+++ binary_trees.h
@@ -79,3 +79,8 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int check_perfect(const binary_tree_t *tree, size_t depth, size_t level);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* 17. Sibling */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
