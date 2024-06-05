daeeeeb807b52db315109d91c2950e00e7cfde18
Stevecmd <murimi101@gmail.com>
2024-06-05 16:06:20 +0300

    tests

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -9,14 +9,14 @@
  */
 size_t height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-    l = tree->left ? 1 + height(tree->left) : 1;
-    r = tree->right ? 1 + height(tree->right) : 1;
-    return ((l > r) ? l : r);
+	l = tree->left ? 1 + height(tree->left) : 1;
+	r = tree->right ? 1 + height(tree->right) : 1;
+	return ((l > r) ? l : r);
 }
 
 /**
@@ -29,21 +29,21 @@ size_t height(const binary_tree_t *tree)
  */
 int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
 {
-    size_t lhgt, rhgt, diff;
+	size_t lhgt, rhgt, diff;
 
-    if (tree != NULL)
-    {
-        if (tree->n <= lo || tree->n >= hi)
-            return (0);
-        lhgt = height(tree->left);
-        rhgt = height(tree->right);
-        diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
-        if (diff > 1)
-            return (0);
-        return (is_avl_helper(tree->left, lo, tree->n) &&
-                is_avl_helper(tree->right, tree->n, hi));
-    }
-    return (1);
+	if (tree != NULL)
+	{
+		if (tree->n <= lo || tree->n >= hi)
+			return (0);
+		lhgt = height(tree->left);
+		rhgt = height(tree->right);
+		diff = lhgt > rhgt ? lhgt - rhgt : rhgt - lhgt;
+		if (diff > 1)
+			return (0);
+		return (is_avl_helper(tree->left, lo, tree->n) &&
+				is_avl_helper(tree->right, tree->n, hi));
+	}
+	return (1);
 }
 
 /**
@@ -54,7 +54,7 @@ int is_avl_helper(const binary_tree_t *tree, int lo, int hi)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (is_avl_helper(tree, INT_MIN, INT_MAX));
+	if (tree == NULL)
+		return (0);
+	return (is_avl_helper(tree, INT_MIN, INT_MAX));
 }
