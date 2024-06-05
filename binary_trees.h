9faa312f6898f5a7fc6f3d133c40d3d71427f04b
Stevecmd <murimi101@gmail.com>
2024-06-05 13:37:48 +0300

    102-binary_tree_is_complete.c update

--- binary_trees.h
+++ binary_trees.h
@@ -102,3 +102,4 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 /* 21. Is complete  */
 int binary_tree_is_complete(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
