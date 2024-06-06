8d678a8c25b8a5c37bb1635bc34be608e1c88ce4
Stevecmd <murimi101@gmail.com>
2024-06-06 12:22:54 +0300

    112-bst_array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -11,15 +11,26 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
-	size_t i;
+	size_t i, j;
 
 	if (array == NULL || size == 0)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		if (bst_insert(&root, array[i]) == NULL)
-			return (NULL);
+		/* Check for duplicate elements */
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+
+		/* If no duplicate found, insert the element into the tree */
+		if (j == i)
+		{
+			if (bst_insert(&root, array[i]) == NULL)
+				return (NULL);
+		}
 	}
 
 	return (root);
